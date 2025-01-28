8e7e7eb0fbae96d24bb46f98f147c66585d21d87
Vansh <vansh.sethi98760@gmail.com>
2025-01-28 19:20:05 +0530

    Permissions Change

--- app/src/main/java/com/example/ble_jetpackcompose/BluetoothScanView.kt
+++ app/src/main/java/com/example/ble_jetpackcompose/BluetoothScanView.kt
@@ -6,9 +6,7 @@ import android.bluetooth.le.ScanResult
 import android.bluetooth.le.ScanSettings
 import android.content.Context
 import android.content.pm.PackageManager
-import android.os.Build
 import android.util.Log
-import androidx.annotation.RequiresApi
 import androidx.core.content.ContextCompat
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
@@ -33,7 +31,6 @@ class BluetoothScanViewModel(private val context: Context) : ViewModel() {
             return
 
 
-
         }
 
 
@@ -55,7 +52,6 @@ class BluetoothScanViewModel(private val context: Context) : ViewModel() {
 
 
 
-    @RequiresApi(Build.VERSION_CODES.S)
     private fun hasBluetoothPermissions(): Boolean {
         val permissions = arrayOf(
             android.Manifest.permission.BLUETOOTH_SCAN,
